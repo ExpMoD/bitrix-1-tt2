1d3e9386e3a69de452d0f0da1b5194a0eb882d1c
Andrey Kudakov <kudakovandrey@gmail.com>
2017-10-28 00:08:03 +0300

    [ex1-15]

--- rew/index.php
+++ rew/index.php
@@ -2,9 +2,9 @@
 require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
 $APPLICATION->SetTitle("Отзывы");
 ?><?$APPLICATION->IncludeComponent(
-	"bitrix:news", 
-	"reviews", 
-	array(
+	"bitrix:news",
+	"reviews",
+	Array(
 		"ADD_ELEMENT_CHAIN" => "N",
 		"ADD_SECTIONS_CHAIN" => "N",
 		"AJAX_MODE" => "N",
@@ -21,18 +21,11 @@ $APPLICATION->SetTitle("Отзывы");
 		"DETAIL_ACTIVE_DATE_FORMAT" => "j F Y",
 		"DETAIL_DISPLAY_BOTTOM_PAGER" => "Y",
 		"DETAIL_DISPLAY_TOP_PAGER" => "N",
-		"DETAIL_FIELD_CODE" => array(
-			0 => "",
-			1 => "",
-		),
+		"DETAIL_FIELD_CODE" => array(0=>"",1=>"",),
 		"DETAIL_PAGER_SHOW_ALL" => "Y",
 		"DETAIL_PAGER_TEMPLATE" => "",
 		"DETAIL_PAGER_TITLE" => "Страница",
-		"DETAIL_PROPERTY_CODE" => array(
-			0 => "POSITION",
-			1 => "COMPANY",
-			2 => "",
-		),
+		"DETAIL_PROPERTY_CODE" => array(0=>"POSITION",1=>"COMPANY",2=>"",),
 		"DETAIL_SET_CANONICAL_URL" => "N",
 		"DISPLAY_BOTTOM_PAGER" => "Y",
 		"DISPLAY_DATE" => "Y",
@@ -45,15 +38,8 @@ $APPLICATION->SetTitle("Отзывы");
 		"IBLOCK_TYPE" => "rews",
 		"INCLUDE_IBLOCK_INTO_CHAIN" => "N",
 		"LIST_ACTIVE_DATE_FORMAT" => "j F Y",
-		"LIST_FIELD_CODE" => array(
-			0 => "",
-			1 => "",
-		),
-		"LIST_PROPERTY_CODE" => array(
-			0 => "POSITION",
-			1 => "COMPANY",
-			2 => "",
-		),
+		"LIST_FIELD_CODE" => array(0=>"",1=>"",),
+		"LIST_PROPERTY_CODE" => array(0=>"POSITION",1=>"COMPANY",2=>"",),
 		"MESSAGE_404" => "",
 		"META_DESCRIPTION" => "-",
 		"META_KEYWORDS" => "-",
@@ -68,6 +54,7 @@ $APPLICATION->SetTitle("Отзывы");
 		"PREVIEW_TRUNCATE_LEN" => "",
 		"SEF_FOLDER" => "/rew/",
 		"SEF_MODE" => "Y",
+		"SEF_URL_TEMPLATES" => array("detail"=>"#ELEMENT_CODE#/","news"=>"","section"=>"",),
 		"SET_LAST_MODIFIED" => "N",
 		"SET_STATUS_404" => "N",
 		"SET_TITLE" => "Y",
@@ -84,13 +71,6 @@ $APPLICATION->SetTitle("Отзывы");
 		"USE_REVIEW" => "N",
 		"USE_RSS" => "N",
 		"USE_SEARCH" => "N",
-		"USE_SHARE" => "N",
-		"COMPONENT_TEMPLATE" => "reviews",
-		"SEF_URL_TEMPLATES" => array(
-			"news" => "",
-			"section" => "",
-			"detail" => "#ELEMENT_CODE#/",
-		)
-	),
-	false
+		"USE_SHARE" => "N"
+	)
 );?><?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
