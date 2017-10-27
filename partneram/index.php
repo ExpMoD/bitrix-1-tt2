b0e0e236f23c89a584249a27485ba2862cf1ee11
Andrey Kudakov <kudakovandrey@gmail.com>
2017-10-27 23:17:56 +0300

    [ex1-7]

--- partneram/index.php
+++ partneram/index.php
@@ -1,8 +1,4 @@
 <?
 require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
 $APPLICATION->SetTitle("Условия сотрудничества");
-?>
-
-Text here....
-
-<?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
+?>Text here....<?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
